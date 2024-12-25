ded37d07dc1dcb90292bbb062135f24461bb8a3b
Lakshya Dhingra <dlakshya223@gmail.com>
2024-12-25 16:57:57 +0530

    Printing definitions from a csv file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import pandas as pd
+
+df = pd.read_csv("dictionary.csv")
+
 
 app = Flask(__name__)
 
@@ -10,8 +14,8 @@ def home():
 
 @app.route("/api/v1/<word>")
 def api(word):
-    new_word = word.upper()
-    return {"definition": new_word, "word": word}
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    return {"definition": definition, "word": word}
 
 
 if __name__ == "__main__":
